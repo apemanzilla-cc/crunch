9b429c55f08831c4e9e243ad6ac309c554c46471
apemanzilla <dominic@marcuse.us>
2018-05-15 22:20:47 -0400

    Rewrite using package lib

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # crunch
-Basic preprocessor that packages libraries in with files
+Crunch is used to bundle folders of code using `require` into a single executable file. This file will then be able to load the embedded modules as if it were in its original directory structure.
 
-Run with the -h argument to see usage
+## Usage
+`crunch <project root> [output file]`
+
+`project root` is the root directory of the project. `main.lua` should exist within this directory, and it will be used as the root for resolving modules when `require` is used. The contents will be recursively embedded into the bundle.
+
+`output file` is the location to write the output to. It defaults to `bundle.lua`.
--- crunch.lua
+++ crunch.lua
@@ -1,156 +1,82 @@
--- crunch.lua
--- written by apemanzilla
--- combines a bunch of files into one runnable file
--- intended to be used to package multiple dependencies together with a program
+local args = { ... }
 
-assert(_HOST or _CC_VERSION, "CC 1.74+ is required")
+if #args < 1 then
+	return error(("Usage: %s <project root> [output file]"):format(fs.getName(shell.getRunningProgram())))
+end
 
-local options = {}
+local root = shell.resolve(args[1])
+local outFile = shell.resolve(args[2] or "bundle.lua")
 
-options.libpath = settings and settings.get("crunch.libpath") or ".:lib:/lib"
-options.libext = settings and settings.get("crunch.libext") or "lua"
+if not fs.isDir(root) then
+	return error("Project root must be a directory")
+end
 
-options.output = true
-options.run = false
+-- recursively collect input files
+local function collectSources(dir)
+	local sources = {}
 
--- parse args
-local args = {...}
-while #args > 0 do
-	local arg = table.remove(args, 1)
-	if arg:sub(1,1) == "-" then
-		-- flag
-		if arg == "-h" or arg == "-?" then
-			print("Usage: crunch [options]")
-			print("Options:")
-			print(" -h: show usage")
-			print(" -n <name>: set the name to use for runtime errors in the main file")
-			print(" -d: do not write output to file")
-			print(" -r: run output (all args following -r will be passed to program)")
-			print("Examples:")
-			print(" 'crunch' - builds program and saves to output.lua")
-			print(" 'crunch -d' - builds program but does not save")
-			print(" 'crunch -d -r myarg' - builds program and runs with argument \"myarg\"")
-			return
-		elseif arg == "-n" then
-			options.name = table.remove(args, 1)
-			if not options.name then
-				error("-n option should be followed by an argument", 0)
+	for i, path in ipairs(fs.list(dir)) do
+		local absPath = fs.combine(dir, path)
+
+		if fs.isDir(absPath) then
+			for k, v in pairs(collectSources(absPath)) do
+				sources[fs.combine(path, k)] = v
 			end
-		elseif arg == "-d" then
-			-- debug flag
-			options.output = false
-		elseif arg == "-r" then
-			options.run = {}
-			while #args > 0 do
-				table.insert(options.run, table.remove(args, 1))
-			end
-		end
-	else
-
-	end
-end
-
-if not options.main then
-	for i, v in ipairs({"main.lua", "main"}) do
-		if fs.exists(fs.combine(shell.dir(), v)) then
-			options.main = fs.combine(shell.dir(), v)
-			break
-		end
-	end
-end
-
-assert(options.main, "no main file present")
-
-local function resolve(base, path)
-	if path:sub(1,1) == "/" or path:sub(1,1) == "\\" then
-		return path
-	else
-		return fs.combine(base, path)
-	end
-end
-
-local function findLib(name, from)
-	for lp in options.libpath:gmatch("([^:]+)") do
-		lp = fs.combine(resolve(from, lp), name)
-		for ext in options.libext:gmatch("([^:]+)") do
-			if fs.exists(lp .. "." .. ext) and not fs.isDir(lp .. "." .. ext) then return lp .. "." .. ext end
-		end
-		if fs.exists(lp) and not fs.isDir(lp) then return lp end
-	end
-end
-
-local pttrn = "@include[ \t]+([%w%d_-%.]+)"
-
-local function crunch(file, loadName)
-	local f = fs.open(file, "r")
-	local data = f.readAll()
-	f.close()
-
-	local included = {}
-	local ct = 0
-	for include in data:gmatch(pttrn) do
-		local lib = findLib(include:gsub("%.", "/"), fs.getDir(file))
-		if lib then
-			included[include] = lib
-			ct = ct + 1
 		else
-			error(("missing library %s, required by %s"):format(include, fs.getName(file)), 0)
+			local f = fs.open(absPath, "r")
+			sources[path] = f.readAll()
+			f.close()
 		end
 	end
 
-	if ct == 0 then
-		-- syntax check
-		local f, e = load(data, fs.getName(file))
-		if not f then
-			error(e, 0)
+	return sources
+end
+
+local sources = collectSources(root)
+if not sources["main.lua"] then printError("Warning: main.lua missing") end
+
+local output = [[
+-- bundle created using crunch
+-- https://github.com/apemanzilla/crunch
+
+]]
+
+-- input contents
+output = output .. "local sources = {\n"
+
+for k, v in pairs(sources) do
+	output = output .. ([[	[%q] = %q,
+]]):format(k, v)
+end
+
+output = output .. "}\n\n"
+
+-- module loader
+output = output .. [[
+assert(package, "package API is required")
+table.insert(package.loaders, 1, function(name)
+	for path in package.path:gmatch("[^;]+") do
+		local test = path:gsub("%?", name:gsub("%.", "/"))
+		if sources[test] then
+			return function(...)
+				return load(sources[test], name, nil, _ENV)(...)
+			end
 		end
-
-		return data
-	else
-		-- syntax check
-		local f, e = load(data:gsub(pttrn, ""), fs.getName(file))
-		if not f then
-			error(e, 0)
-		end
-
-		local loaders = {}
-
-		for k, v in pairs(included) do
-			local crunched = ("%q"):format(crunch(v))
-			local name = k:match("([^%.]+)$")
-			if loaders[name] then error("name conflict: " .. name, 0) end
-			loaders[name] = crunched
-			data = data:gsub("@include[ \t]+("..k..")", "")
-			--data = data:gsub("@include[ \t]+("..k..")", "do local e = setmetatable({}, {__index = _G}) e._ENV = e _ENV[\""..name.."\"] = load("..crunched..", \""..name.."\", nil, e)() end")
-		end
-
-		local output = ""
-
-		-- env building
-		for k, v in pairs(loaders) do
-			output = output .. "do local e = setmetatable({}, {__index = _ENV}) e._ENV = e _ENV[\""..k.."\"] = load("..v..", \""..k.."\", nil, e)() end\n"
-		end
-
-		data = ("%q"):format(data)
-		output = output .. "\nreturn load(" .. data .. ", \"" .. (loadName or fs.getName(file)) .. "\", nil, _ENV)(...)"
-
-		return output
 	end
-end
 
-local header = "-- this code was generated using crunch by apemanzilla\n-- you should probably look for the source code elsewhere before attempting to read this file\nassert(_HOST or _CC_VERSION, \"CC 1.74+ is required.\")\n\n"
-local output = crunch(options.main, options.name)
+	return nil, "no matching embedded file"
+end)
 
-if options.output then
-	local f = fs.open(fs.combine(fs.getDir(options.main), "output.lua"), "w")
-	f.write(header .. output)
-	f.close()
-end
+]]
 
-if options.run then
-	local env = setmetatable({}, {__index = _ENV}) env._ENV = env
-	local f, e = load(output, fs.getName(options.main), nil, env)
-	if not f then error(e, 0) end
-	local ok, e = pcall(f, unpack(options.run))
-	if not ok then error(e, 0) end
-end
+-- launcher
+output = output .. [[
+load(sources["main.lua"] or "", "main.lua", nil, _ENV)(...)
+]]
+
+-- write output
+local f = fs.open(outFile, "w")
+f.write(output)
+f.close()
+
+print("Output successfully written to " .. outFile)
